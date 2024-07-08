2090d6905394923a39d4219138bebf0ffa27b6d6
Matheus C Marques <matheuscamarques@gmail.com>
2024-07-08 14:07:55 -0300

    fix: clear sandbox keys

--- config/sandbox.exs
+++ config/sandbox.exs
@@ -1,5 +1,5 @@
 config :pomelo_ex,
-  client_id: "3LBFuOiEHrl4BailkRlsnIMmSctMLb7A",
-  client_secret: "s5u3oYK0-A9CV6TkszPQThUa_qxMfr2yyd3-eJwQ...",
-  audience: "https://auth-dev.pomelo.la",
-  grant_type: "client_credentials"
+  client_id: "",
+  client_secret: "",
+  audience: "",
+  grant_type: ""
