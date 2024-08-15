d23d60c4de0659a7a30239d29d0ee0d8ef438ecc
August Moreno <camilomoreno1488@live.com>
2024-08-14 19:22:02 -0500

    feat: Cards credits webhooks interfaces

---
+++ lib/pomelo_ex/cards/credits/webhooks/credit_line_pause_unpause_notifications.ex
@@ -0,0 +1,5 @@
+defmodule PomeloEx.Cards.Credits.Webhooks.CreditLinePauseUnpauseNotifications do
+  def execute(payload) do
+    raise "Not implemented #{__MODULE__} payload:" <> inspect(payload)
+  end
+end
---
+++ lib/pomelo_ex/cards/credits/webhooks/entry_exit_from_arrears_user_notifications.ex
@@ -0,0 +1,5 @@
+defmodule PomeloEx.Cards.Credits.Webhooks.EntryExitFromArrearsUserNotifications do
+  def execute(payload) do
+    raise "Not implemented #{__MODULE__} payload:" <> inspect(payload)
+  end
+end
