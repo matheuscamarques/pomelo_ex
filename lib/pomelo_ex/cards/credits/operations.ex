d9d01b16e1daf7eaa88661f3b8ecfa4cf25f74de
August Moreno <camilomoreno1488@live.com>
2024-08-18 12:04:06 -0500

    feat: Docs for Cards credits operations

--- lib/pomelo_ex/cards/credits/operations.ex
+++ lib/pomelo_ex/cards/credits/operations.ex
@@ -3,7 +3,13 @@ defmodule PomeloEx.Cards.Credits.Operations do
   This service allows you to query the performed operations.
   """
 
-  alias PomeloEx.Cards.Credits.Operations.GetOperations
+  @doc """
+  Get operation
+  The /operations endpoint allows you to perform a search with specific filters.
 
+  Filters#
+  You can find more information on how to apply filters in the section [Filters and pagination](https://developers.pomelo.la/api-reference/filters).
+  """
+  alias PomeloEx.Cards.Credits.Operations.GetOperations
   defdelegate get_operations(payload), to: GetOperations, as: :execute
 end
