a11547f50178295b5b6416ec382d8e0ac6756218
August Moreno <camilomoreno1488@live.com>
2024-08-20 11:58:50 -0500

    fix: credo Fraud prevention travel notice

--- lib/pomelo_ex/fraud_prevention/travel_notice/obtain_travel_notification.ex
+++ lib/pomelo_ex/fraud_prevention/travel_notice/obtain_travel_notification.ex
@@ -1,4 +1,6 @@
 defmodule PomeloEx.FraudPrevention.TravelNotice.ObtainTravelNotification do
+  @moduledoc false
+
   def execute(payload) do
     raise "Not implemented #{__MODULE__} payload:" <> inspect(payload)
   end
