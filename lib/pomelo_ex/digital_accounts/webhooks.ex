20dc44a216ba98c1331ab95135f0024bad724b4f
August Moreno <camilomoreno1488@live.com>
2024-08-20 11:47:10 -0500

    fix: credo Digital accounts webhooks

--- lib/pomelo_ex/digital_accounts/webhooks.ex
+++ lib/pomelo_ex/digital_accounts/webhooks.ex
@@ -10,11 +10,11 @@ defmodule PomeloEx.DigitalAccounts.Webhooks do
 
   You can also find more information about the [Verification Process](https://docs.pomelo.la/docs/developers/key-exchange) of the digital signature.
   """
+  alias PomeloEx.DigitalAccounts.Webhooks.ActivityNotification
 
   @doc """
   Activity notification
   The following endpoint must be in the client service so that it can receive the requests of the activities created/updated. If the request returns an HTTP code of type 2xx, it will not be sent again and will be marked as Sent correctly. Otherwise, we will try again.
   """
-  alias PomeloEx.DigitalAccounts.Webhooks.ActivityNotification
   defdelegate activity_notification(payload), to: ActivityNotification, as: :execute
 end
