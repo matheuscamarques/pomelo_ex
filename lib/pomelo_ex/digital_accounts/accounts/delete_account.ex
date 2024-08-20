848b71a4cab0acd80a0659bd617a18621351ad7c
August Moreno <camilomoreno1488@live.com>
2024-08-20 11:45:17 -0500

    fix: credo Digital accounts accounts

--- lib/pomelo_ex/digital_accounts/accounts/delete_account.ex
+++ lib/pomelo_ex/digital_accounts/accounts/delete_account.ex
@@ -1,4 +1,6 @@
 defmodule PomeloEx.DigitalAccounts.Accounts.DeleteAccount do
+  @moduledoc false
+
   def execute(payload) do
     raise "Not implemented #{__MODULE__} payload:" <> inspect(payload)
   end
