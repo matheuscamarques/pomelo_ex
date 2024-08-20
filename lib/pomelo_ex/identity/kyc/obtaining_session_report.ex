8710529ee5fcaee73afc9f15b0612cbbd3112171
August Moreno <camilomoreno1488@live.com>
2024-08-20 11:51:19 -0500

    fix: credo Identity KYC

--- lib/pomelo_ex/identity/kyc/obtaining_session_report.ex
+++ lib/pomelo_ex/identity/kyc/obtaining_session_report.ex
@@ -1,4 +1,6 @@
 defmodule PomeloEx.Identity.KYC.ObtainingSessionReport do
+  @moduledoc false
+
   def execute(payload) do
     raise "Not implemented #{__MODULE__} payload:" <> inspect(payload)
   end
