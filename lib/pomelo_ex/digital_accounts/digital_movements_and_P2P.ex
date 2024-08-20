09b9e1a79ef919fef052b20cf8c70bc06f4930f3
August Moreno <camilomoreno1488@live.com>
2024-08-20 11:46:50 -0500

    fix: credo Digital accounts digital movements and P2P

--- lib/pomelo_ex/digital_accounts/digital_movements_and_P2P.ex
+++ lib/pomelo_ex/digital_accounts/digital_movements_and_P2P.ex
@@ -1,15 +1,17 @@
 defmodule PomeloEx.DigitalAccounts.DigitalMovementsAndP2P do
+  @moduledoc false
+  alias PomeloEx.DigitalAccounts.DigitalMovementsAndP2P.AuthorizeDigitalMovements
+  alias PomeloEx.DigitalAccounts.DigitalMovementsAndP2P.AuthorizeP2PTransaction
+
   @doc """
   Authorize Digital Movements
   The endpoint /core/transactions/v1 allows you to authorize incoming and outgoing money transactions from digital accounts. Check the field result to see the result.
   """
-  alias PomeloEx.DigitalAccounts.DigitalMovementsAndP2P.AuthorizeDigitalMovements
   defdelegate authorize_digital_movements(payload), to: AuthorizeDigitalMovements, as: :execute
 
   @doc """
   Autorizar Transacción P2P
   El endpoint /core/transactions/v1/p2p te permite autorizar movimientos de terceros (P2P) de entrada y salida de dinero entre dos cuentas de Pomelo. Podrás consultar el resultado de la transacción en el campo result.
   """
-  alias PomeloEx.DigitalAccounts.DigitalMovementsAndP2P.AuthorizeP2PTransaction
   defdelegate authorize_P2P_transaction(payload), to: AuthorizeP2PTransaction, as: :execute
 end
