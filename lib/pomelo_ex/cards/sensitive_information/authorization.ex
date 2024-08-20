27cd3a5285a3f29b61e176d798b703e2e2d577a6
August Moreno <camilomoreno1488@live.com>
2024-08-20 11:22:40 -0500

    fix: credo Cards sensitive information

--- lib/pomelo_ex/cards/sensitive_information/authorization.ex
+++ lib/pomelo_ex/cards/sensitive_information/authorization.ex
@@ -2,12 +2,11 @@ defmodule PomeloEx.Cards.SensitiveInformation.Authorization do
   @moduledoc """
   With this API you can authenticate a user to display sensitive card information.
   """
+  alias PomeloEx.Cards.SensitiveInformation.Authorization.CreateUserToken
 
   @doc """
   Create user token
   This endpoint lets you create a token for a specific user, which is used to display your card's secure information. Use the token obtained in the Authorization section and keep in mind that it only lasts fifteen minutes.
   """
-  alias PomeloEx.Cards.SensitiveInformation.Authorization.CreateUserToken
-
   defdelegate create_user_token(payload), to: CreateUserToken, as: :execute
 end
--- lib/pomelo_ex/cards/sensitive_information/handling/activate_card.ex
+++ lib/pomelo_ex/cards/sensitive_information/handling/activate_card.ex
@@ -1,4 +1,6 @@
 defmodule PomeloEx.Cards.SensitiveInformation.Handling.ActivateCard do
+  @moduledoc false
+
   def execute(payload) do
     raise "Not implemented #{__MODULE__} payload:" <> inspect(payload)
   end
