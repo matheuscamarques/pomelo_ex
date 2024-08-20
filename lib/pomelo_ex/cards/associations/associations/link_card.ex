6a1bbb4510765c13c0a8cddf360442da8f6dbfc5
August Moreno <camilomoreno1488@live.com>
2024-08-20 11:18:30 -0500

    fix: credo Cards associations

--- lib/pomelo_ex/cards/associations/associations/link_card.ex
+++ lib/pomelo_ex/cards/associations/associations/link_card.ex
@@ -1,4 +1,6 @@
 defmodule PomeloEx.Cards.Associations.Associations.LinkCard do
+  @moduledoc false
+
   def execute(payload) do
     raise "Not implemented #{__MODULE__} payload:" <> inspect(payload)
   end
