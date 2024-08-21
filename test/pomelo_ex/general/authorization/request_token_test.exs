f788e249ea1e2104188a136fed9a9839602a4954
Matheus C Marques <matheuscamarques@gmail.com>
2024-08-21 15:18:08 -0300

    feat: request token test

---
+++ test/pomelo_ex/general/authorization/request_token_test.exs
@@ -0,0 +1,9 @@
+defmodule PomeloEx.General.Authorization.RequestTokenTest do
+	use ExUnit.Case
+	doctest PomeloEx.General.Authorization.RequestToken
+	alias PomeloEx.General.Authorization.RequestToken
+
+  test "Sucess 200 - Authenticate Token" do
+    assert PomeloEx.General.Authorization.RequestToken.execute() == true
+  end
+end
--- test/pomelo_ex_test.exs
+++ test/pomelo_ex_test.exs
@@ -2,7 +2,4 @@ defmodule PomeloExTest do
   use ExUnit.Case
   doctest PomeloEx
 
-  test "greets the world" do
-    assert PomeloEx.hello() == :world
-  end
 end
