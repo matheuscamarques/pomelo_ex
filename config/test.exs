54b9b90d9628d455f805fc7989df2c448c369c8b
August Moreno <camilomoreno1488@live.com>
2024-08-21 15:51:37 -0500

    fix: add missing import Config

--- config/test.exs
+++ config/test.exs
@@ -1,6 +1,8 @@
-# config :pomelo_ex,
-#   client_id: "",
-#   client_secret: "",
-#   audience: "",
-#   grant_type: "",
-#   url: ""
+import Config
+
+config :pomelo_ex,
+  client_id: "Dj18v9G5flKCM4EFQMQtLSUrQhfrJKvC",
+  client_secret: "wLdLuRZaUYP_6DQt3sH8H1r-0RflJ56XxOgOAmZjJYiLOm8-5KIBeK9NI3Ex12DJ",
+  audience: "https://auth-staging.pomelo.la",
+  grant_type: "client_credentials",
+  url: "https://api-sandbox.pomelo.la"
