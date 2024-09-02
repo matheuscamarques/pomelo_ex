3a15ba46eb4f91297e664cd01c20db847a88228c
August Moreno <camilomoreno1488@live.com>
2024-09-02 18:14:57 -0500

    feat: add test/fixtures to compilation

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule PomeloEx.MixProject do
       app: :pomelo_ex,
       version: "0.1.0",
       elixir: "~> 1.16",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -18,6 +19,10 @@ defmodule PomeloEx.MixProject do
       mod: {PomeloEx.Application, []}
     ]
   end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/fixtures"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
