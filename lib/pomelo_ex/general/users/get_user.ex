a049046989df6c5e419b869401c3d7f663a42a82
August Moreno <camilomoreno1488@live.com>
2024-09-03 15:14:56 -0500

    feat: get user

--- lib/pomelo_ex/general/users/get_user.ex
+++ lib/pomelo_ex/general/users/get_user.ex
@@ -1,7 +1,31 @@
 defmodule PomeloEx.General.Users.GetUser do
   @moduledoc false
+  use TypedEctoSchema
 
-  def execute(payload) do
-    raise "Not implemented #{__MODULE__} payload:" <> inspect(payload)
+  alias PomeloEx.General.Authorization
+
+  typed_embedded_schema do
+  end
+
+  def execute(%__MODULE__{id: id}) do
+    http_client = Application.get_env(:pomelo_ex, :http_adapter)
+    url = Application.get_env(:pomelo_ex, :url)
+    headers = build_headers()
+
+    http_client.get("#{url}/users/v1/#{id}", headers)
+  end
+
+  defp build_headers do
+    {:ok, %HTTPoison.Response{status_code: 200, body: body}} = Authorization.request_token()
+
+    token =
+      body
+      |> Jason.decode!()
+      |> Map.fetch!("access_token")
+
+    [
+      {"Content-Type", "application/json"},
+      {"Authorization", "Bearer #{token}"}
+    ]
   end
 end
