cbe6af3e365d433b738d4087f1bdbc6f9da6bbb4
August Moreno <camilomoreno1488@live.com>
2024-09-03 15:14:34 -0500

    fix: search user test

--- test/pomelo_ex/general/users/search_user_test.exs
+++ test/pomelo_ex/general/users/search_user_test.exs
@@ -6,19 +6,26 @@ defmodule PomeloEx.General.Users.SearchUserTest do
   alias PomeloEx.General.UsersFixtures
 
   test "Success 200 - Search User" do
-    expect(HTTPMock, :post, fn url, _body, _headers ->
-      assert url == Application.get_env(:pomelo_ex, :url) <> "/users/v1/"
+    expect(HTTPMock, :get, fn url, _headers ->
+      assert url == Application.get_env(:pomelo_ex, :url) <> "/users/v1/?"
 
       {:ok,
        %HTTPoison.Response{
          status_code: 200,
          body: UsersFixtures.search_user_response()
        }}
-
-      {:ok, response} = SearchUser.execute(%SearchUser{})
-
-      body = Jason.decode!(response.body)
-      assert body["data"]["id"] == "usr-2lWyhVOQarR8I5te2XNXdMleSEu"
     end)
+
+    {:ok, response} = SearchUser.execute(%SearchUser{})
+
+    body = Jason.decode!(response.body)
+
+    user_id =
+      body
+      |> Map.fetch!("data")
+      |> List.first()
+      |> Map.fetch!("id")
+
+    assert user_id == "usr-2lWyhVOQarR8I5te2XNXdMleSEu"
   end
 end
