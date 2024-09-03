d7e364950bf630d136bdf753fe25dcdaa93d9deb
August Moreno <camilomoreno1488@live.com>
2024-09-03 15:15:16 -0500

    feat: get user test

--- test/fixtures/general/users_fixtures.ex
+++ test/fixtures/general/users_fixtures.ex
@@ -1,6 +1,7 @@
 defmodule PomeloEx.General.UsersFixtures do
   alias PomeloEx.General.Users.CreateUser
   alias PomeloEx.General.Users.CreateUser.LegalAddress
+  alias PomeloEx.General.Users.GetUser
 
   def create_user_request do
     %CreateUser{
@@ -28,6 +29,10 @@ defmodule PomeloEx.General.UsersFixtures do
       },
       operation_country: "BRA"
     }
+  end
+
+  def get_user_request do
+    %GetUser{id: "usr-2lWyhVOQarR8I5te2XNXdMleSEu"}
   end
 
   def create_user_response do
@@ -104,6 +109,41 @@ defmodule PomeloEx.General.UsersFixtures do
               "current_page": 0
             },
             "filters": []
+          }
+        }
+    |
+  end
+
+  def get_user_response do
+    ~s|
+        {
+          "data": {
+            "id": "usr-2lWyhVOQarR8I5te2XNXdMleSEu",
+            "name": "Diego",
+            "surname": "Pomelo",
+            "identification_type": "RG",
+            "identification_value": "42345678",
+            "birthdate": "1998-08-20",
+            "gender": "MALE",
+            "email": "diego.pomelo@pomelo.la",
+            "phone": "1123456789",
+            "status": "ACTIVE",
+            "operation_country": "BRA",
+            "legal_address": {
+              "floor": "1",
+              "street_name": "Av. Corrientes",
+              "street_number": "300",
+              "apartment": "A",
+              "city": "Ilhéus-Itabuna",
+              "region": "BA",
+              "zip_code": "4545041",
+              "neighborhood": "Villa Crespo",
+              "country": "BRA",
+              "additional_info": "Torre 2"
+            },
+            "client_id": "cli-2ijPOgbwKFkmUjEnE4vM9IMea78",
+            "nationality": "BRA",
+            "tax_condition": "VAT_REGISTERED"
           }
         }
     |
